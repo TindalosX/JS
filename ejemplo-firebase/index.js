e8da9cdcf4aac491ea7e9134a8b027c49598082a
5thDemensionOfBugs <5thDemensionOfBugs@ejemplo.com>
2025-07-29 20:11:56 -0600

    Creación de app con Firebase

---
+++ ejemplo-firebase/index.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  //Aquí van los datos del objeto de configuración para la app.
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log(app);
