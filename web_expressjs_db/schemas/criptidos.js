9be72308f0498f43859e3cb57c011424e2d6ad17
5thDemensionOfBugs <5thDemensionOfBugs@ejemplo.com>
2025-09-19 00:52:14 -0600

    Se elimino el manejo del id en el objeto y se modificaron los mensajes para nombre

--- web_expressjs_db/schemas/criptidos.js
+++ web_expressjs_db/schemas/criptidos.js
@@ -2,26 +2,26 @@
 import Joi from 'joi'
 
 const schemaCriptido = Joi.object({
-	id: Joi.string().alphanum().min(6).max(16).
-		required().
-		messages({
-			'string.min': `id debe tener un mínimo de {#limit} caracteres`,
-			'string.max': `id debe tener un máximo de {#limit} caracteres`,
-			'any.required': `id es un campo obligatorio.`,
-			'string.empty': `id no puede ser vacío.`
-	}),
-	name: Joi.string().min(6).max(15).
+	//~ id: Joi.string().alphanum().min(6).max(16).
+		//~ required().
+		//~ messages({
+			//~ 'string.min': `id debe tener un mínimo de {#limit} caracteres`,
+			//~ 'string.max': `id debe tener un máximo de {#limit} caracteres`,
+			//~ 'any.required': `id es un campo obligatorio.`,
+			//~ 'string.empty': `id no puede ser vacío.`
+	//~ }),
+	nombre: Joi.string().min(6).max(15).
 		alter({
 			post: (schemaCriptido) => schemaCriptido.required(),
 			patch: (schemaCriptido) => schemaCriptido.optional(),
 		}).
 		messages({
-			'string.min': 'nombre debe tener al menos 6 caracteres',
-			'string.max': 'nombre debe tener un máximo de {#limit} caracteres',
-			'string.empty': 'nombre no puede ser vacío.',
-			'any.required': 'nombre: es un campo obligatorio.'
+			'string.min': 'El nombre debe tener al menos 6 caracteres',
+			'string.max': 'El nombre debe tener un máximo de {#limit} caracteres',
+			'string.empty': 'Introduce un nombre por favor.',
+			'any.required': 'Nombre: es un campo obligatorio.'
 		}),
-	type: Joi.array().
+	tipo: Joi.array().
 		items(Joi.string().valid('acuatico', 'terrestre', 'volador', 'mitologico')).
 		min(1).message({ 'array.min': "Se debe seleccionar al menos un tipo."}).
 		max(3).message({ 'array.max': "No se aceptan más de 3 tipos."}).
